06090902ce43e9e0be91ca55b3f9a35d259b88a3
Miller Vasco <millermartinsvasco1@gmail.com>
2022-06-28 20:35:15 -0300

    Aula - Lendo os items - concluida

--- todo/src/app/app.component.ts
+++ todo/src/app/app.component.ts
@@ -20,6 +20,8 @@ export class AppComponent {
         Validators.required,
       ])]
     });
+
+    this.load();
   }
 
   add(){
@@ -39,18 +41,26 @@ export class AppComponent {
     if(index !== -1){
       this.todos.splice(index, 1);
     }
+    this.save()
   }
 
   markAsDone(todo: Todo){
     todo.done = true;
+    this.save();
   }
 
   markAsUndone(todo: Todo){
     todo.done = false;
+    this.save();
   }
 
   save(){
-    const data=JSON.stringify(this.todos);
+    const data = JSON.stringify(this.todos);
     localStorage.setItem('todos', data);
+  }
+
+  load(){
+    const data = localStorage.getItem('todos');
+    //this.todos = JSON.parse(data);
   }
 }
